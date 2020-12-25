5ae71ff7876b6a597e269e7e0853561aff44d54d
Harpreet Singh <hsingh9600@gmail.com>
2020-12-26 10:40:11 +1300

    test commit

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Homepage from './pages/Homepage'
 
 export default function App() {
   return (
     <View style={styles.container}>
+      < Homepage />
       <Text>Open up App.tsx to start working on your app!</Text>
     </View>
   );
